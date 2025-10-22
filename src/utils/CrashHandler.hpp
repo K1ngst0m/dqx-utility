cc036e75792acde1df439e788cc812d8fcc02c8e
Kingstom <kingstom.chen@gmail.com>
2025-10-22 12:56:53 +0800

    feat: Add CrashHandler documentation

--- src/utils/CrashHandler.hpp
+++ src/utils/CrashHandler.hpp
@@ -5,12 +5,26 @@
 namespace utils
 {
 
+/**
+ * Crash handler for unhandled exceptions and fatal errors.
+ *
+ * Captures crashes, generates stack traces via cpptrace, 
+ * and writes minidump files (windows).
+ * Also intercepts std::terminate() calls.
+ */
 class CrashHandler
 {
 public:
+    /// Installs exception handlers
     static void Initialize();
+
+    /// Sets thread-local context string to be included in crash reports
     static void SetContext(const char* operation);
+
+    /// Registers an atomic flag to be set to true on fatal error
     static void RegisterFatalFlag(std::atomic<bool>* flag);
+
+    /// Registers a cleanup function to be called before crash termination (e.g., flush buffers)
     static void RegisterFatalCleanup(void (*fn)());
 };
 
